8ef0b5e55e40f29cb59ed04d9736190c5a4ee023
Daniel Ibanga <37911129+Dannieib@users.noreply.github.com>
2018-08-29 18:35:22 +0100

    Add files via upload
    How to define an array in C sharp

---
+++ MyArray/MyArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // C Sharp program to make array of numbers
+            int[] list = { 34, 72, 13, 44, 25, 30, 10 };
+            int[] temp = list;
+            Console.Write("original array: ");
+            foreach( int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+            //reversed array of numbers
+            Array.Reverse(temp);
+            Console.Write("reversed Array ");
+            foreach(int i in temp)
+            {
+                Console.Write(i + " ");
+            
+            } 
+            //sorted array of numbers
+            Array.Sort(list);
+            Console.WriteLine("Sorted arrays: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
